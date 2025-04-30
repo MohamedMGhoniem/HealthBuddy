b5a2989e7a54f60e8f2084b7fe629319fcf42e78
MohamedMGhoniem <ghoniemmohamed416@gmail.com>
2025-04-30 17:16:20 +0300

    HeroSection updated

--- src/hero/HeroSection.js
+++ src/hero/HeroSection.js
@@ -2,6 +2,9 @@ import LaptopSvg from './LaptopSvg';
 import MobileSvg from './MobileSvg';
 import Section from '../Section';
 import Pulse from './Pulse';
+import ContentParagraph from '../ContentParagraph';
+import ContentHeading from '../ContentHeading';
+import Content from '../Content';
 
 export default function HeroSection() {
   return (
@@ -12,29 +15,32 @@ export default function HeroSection() {
         aria-hidden="true"
         className="decoration-top"
       ></img>
-      <div className="content">
-        <h1 className="heading-primary">
-          Your Smart Health <br /> Companion
-        </h1>
-        <span className="subheading margin-bot-lrg">
-          Personalized Health Starts Here
-        </span>
-        <p className="paragraph content-txt">
+      <Content>
+        <ContentHeading
+          isHero={true}
+          title={
+            <>
+              Your Smart Health <br /> Companion
+            </>
+          }
+          subheading={'Personalized Health Starts Here'}
+        />
+        <ContentParagraph>
           Meet MediBuddy — your AI-powered wellness companion that understands
           your body, your goals, and your lifestyle. Get custom meal plans,
           nutrition insights, and smart daily guidance made just for you.
-        </p>
-        <LaptopSvg></LaptopSvg>
-        <MobileSvg></MobileSvg>
-        <button className="btn-download">Download Now!</button>
-      </div>
+        </ContentParagraph>
+        <LaptopSvg />
+        <MobileSvg />
+      </Content>
+      <button className="btn-download">Download Now!</button>
+      <Pulse />
       <img
         src="./images/hero/hero-bottom.png"
         alt="decoration"
         aria-hidden="true"
         className="decoration-bottom"
       ></img>
-      <Pulse />
     </Section>
   );
 }
