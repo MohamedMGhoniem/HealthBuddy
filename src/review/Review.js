d1f9d8c50f1d675db9792aa46753b929613ec83a
MohamedMGhoniem <ghoniemmohamed416@gmail.com>
2025-05-09 01:15:17 +0300

    few adjustment

--- src/review/Review.js
+++ src/review/Review.js
@@ -3,17 +3,22 @@ import Content from '../Content';
 import ContentHeading from '../ContentHeading';
 import TestimonialCarousel from './TestimonialCarousel';
 import '../styles/review.css';
+import ContentParagraph from '../ContentParagraph';
+import Decoration from '../Decoration';
 
 export default function Review() {
   return (
     <Section className={'section-review'}>
+      <Decoration
+        src={'./images/review/decoration-back.png'}
+        className={'decoration-back'}
+      />
       <Content>
-        <ContentHeading
-          title={'Healthy stories worth sharing'}
-          subheading={
-            'Real stories, real results — here’s how MediBuddy is helping people feel better, eat smarter, and take charge of their health every day.'
-          }
-        />
+        <ContentHeading title={'Healthy stories worth sharing'} />
+        <ContentParagraph>
+          Real stories, real results — here’s how MediBuddy is helping people
+          feel better, eat smarter, and take charge of their health every day.
+        </ContentParagraph>
       </Content>
       <div className="container">
         <TestimonialCarousel />
